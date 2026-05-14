5105199ab61d2627f051684ce48aa3827f315af6
Stanley Buenaventura <105530737+SBSonk@users.noreply.github.com>
2026-05-14 22:04:17 +0800

    fixed ping and player count shows for all

--- Assets/Scripts/ServerUIDisplay.cs
+++ Assets/Scripts/ServerUIDisplay.cs
@@ -2,19 +2,48 @@ using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 
-public class ServerUIDisplay : MonoBehaviour
+public class ServerUIDisplay : NetworkBehaviour
 {
+    public NetworkVariable<int> playerCount = new NetworkVariable<int>();
     public TextMeshProUGUI playerCountText, pingText;
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer) {
+            playerCount.Value = 0;
+        
+            NetworkManager.OnClientConnectedCallback += PlayerConnected;
+            NetworkManager.OnClientDisconnectCallback += PlayerDisconnected;
+        } 
+
+        playerCount.OnValueChanged += UpdatePlayerCount;
+
+        UpdatePlayerCount(0, playerCount.Value);
+    }
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        NetworkManager.OnClientConnectedCallback -= PlayerConnected;
+        NetworkManager.OnClientDisconnectCallback -= PlayerDisconnected;
+
+        if (!IsServer) 
+            playerCount.OnValueChanged -= UpdatePlayerCount;
+    }
+
+    void PlayerConnected(ulong _) => playerCount.Value++;
+    void PlayerDisconnected(ulong _) => playerCount.Value--;
 
     void Update()
     {
-        playerCountText.gameObject.SetActive(NetworkManager.Singleton.IsServer);
-        if (NetworkManager.Singleton.IsServer) 
-        {
-            playerCountText.text = $"Players: {NetworkManager.Singleton.ConnectedClients.Count}";
-        }
+        if (!NetworkManager.IsListening) return;
 
         ulong ping = NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetCurrentRtt(NetworkManager.Singleton.NetworkConfig.NetworkTransport.ServerClientId);
         pingText.text = $"Ping: {ping}ms";
+    }
+
+    void UpdatePlayerCount(int prev, int newCount)
+    {
+        playerCountText.text = $"Players: {newCount}";
     }
 }
